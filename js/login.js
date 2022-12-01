17bc33877cbf62d46e87e260a2041b377f2d21ae
HYEMBURGER <burgerhyem@gmail.com>
2022-12-01 15:12:50 +0900

    📌 stop refresh, greeting, hidden input

---
+++ js/login.js
@@ -0,0 +1,16 @@
+const idForm = document.querySelector("#id-form");
+const idInput = document.querySelector("#id-input");
+const greeting = document.querySelector("#greeting");
+
+// stop Refresh
+function submitEvent(event) {
+  event.preventDefault();
+
+  // write greeting
+  greeting.innerText = `Hello ${idInput.value} ! Welcome to this page !`;
+
+  // hidden input
+  idForm.classList.add("hidden");
+}
+
+idForm.addEventListener("submit", submitEvent);
